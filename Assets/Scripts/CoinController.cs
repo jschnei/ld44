e8c2751188a216802c12adc88ce76ec23d1929aa
Jon Schneider <jschneider2013@gmail.com>
2019-04-27 12:08:33 -0400

    move coin left/right

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 position = rigidbody2d.position;
+        position.x += 3.0f * horizontal * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
