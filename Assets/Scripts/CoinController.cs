93835623f5b3b056d2fd818b57f926beb58ea741
Jon Schneider <jschneider2013@gmail.com>
2019-04-27 13:24:47 -0400

    switch between different coins

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CoinController : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
+    public bool activeCoin = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +16,25 @@ public class CoinController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!activeCoin) return;
+
         float horizontal = Input.GetAxis("Horizontal");
 
         Vector2 position = rigidbody2d.position;
         position.x += 3.0f * horizontal * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    public void Activate()
+    {
+        Debug.Log("Activating!");
+        activeCoin = true;
+    }
+
+    public void Deactivate()
+    {
+        Debug.Log("Deactivating!");
+        activeCoin = false;
     }
 }
