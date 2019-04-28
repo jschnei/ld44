38d4f982df244e069577576ac31e5e3d1aeed7b6
Jon Schneider <jschneider2013@gmail.com>
2019-04-28 02:48:41 -0400

    camera follows

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+
+    Vector3 offsetZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+
+        transform.position = target.transform.position + offsetZ;
+    }
+
+    public void FocusTarget(GameObject newTarget)
+    {
+        Debug.Log("Focusing camera!");
+
+        offsetZ = new Vector3(0, 0, transform.position.z - newTarget.transform.position.z);
+        target = newTarget;
+
+    }
+}
