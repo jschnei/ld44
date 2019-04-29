09a2db5221efbb865c54a76565b722a12aa2a12b
Jon Schneider <jschneider2013@gmail.com>
2019-04-29 01:17:10 -0400

    signs

---
+++ Assets/Scripts/SignController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignController : MonoBehaviour
+{
+    int coinsOnSign = 0;
+
+    GameObject signText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        signText = transform.Find("SignText").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ReconfigureUI()
+    {
+        if(coinsOnSign > 0)
+        {
+            signText.GetComponent<MeshRenderer>().enabled = true;
+        } else
+        {
+            signText.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        CoinController coin = collider.gameObject.GetComponent<CoinController>();
+        if (coin == null) return;
+
+        coinsOnSign += 1;
+
+        ReconfigureUI();
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        CoinController coin = collider.gameObject.GetComponent<CoinController>();
+        if (coin == null) return;
+
+        coinsOnSign -= 1;
+
+        ReconfigureUI();
+    }
+}
