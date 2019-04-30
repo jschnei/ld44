bd14609b757d6cf4c9b99498b78cdce94a881831
Jon Schneider <jschneider2013@gmail.com>
2019-04-29 21:41:38 -0400

    background music

---
+++ Assets/Scripts/BackgroundMusicController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicController : MonoBehaviour
+{
+    //public AudioClip backgroundMusic;
+
+    private static BackgroundMusicController instance = null;
+    public static BackgroundMusicController Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
